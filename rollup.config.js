0b7163bfea5d6591670d2802a7c6cf0348df124e
Enrico Valbuena <enrico@valpunk.com>
2020-01-23 11:47:32 -0800

    removed a rollup typescript issue

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import typescript from "rollup-plugin-typescript2"
+import typescript from "@rollup/plugin-typescript"
 import commonjs from "rollup-plugin-commonjs"
 import external from "rollup-plugin-peer-deps-external"
 import resolve from "rollup-plugin-node-resolve"
@@ -25,9 +25,9 @@ export default {
     external(),
     resolve(),
     typescript({
-      rollupCommonJSResolveHack: true,
-      exclude: "**/__tests__/**",
-      clean: true
+      // rollupCommonJSResolveHack: true,
+      exclude: ["**/__tests__/**", "__tests__"]
+      // clean: true
     }),
     commonjs({
       include: ["node_modules/**"],
